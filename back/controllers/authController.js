9c9e7cf8d4ecf0973f4ed44498cce805ed110040
nicolas <nicolas.mercier570@gmail.com>
2020-12-14 17:37:04 +0100

    handling errors

---
+++ back/controllers/authController.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const connection = require('../database/conf');
+
+const AuthController = {};
+
+AuthController.register = (req, res) => {
+  const data = req.body;
+  const sql = 'INSERT INTO user SET ?';
+
+  return connection.query(sql, data, (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        message: 'Something went wrong',
+        data: {},
+        error: err.message,
+      });
+    }
+    return connection.query(
+      'SELECT * FROM user WHERE id = ?',
+      results.insertId,
+      (err2, records) => {
+        if (err2) {
+          return res.status(500).json({
+            success: false,
+            message: 'Something went wrong',
+            data: {},
+            error: err2.message,
+          });
+        }
+        const newUser = records[0];
+        return res.status(201).json({
+          success: true,
+          message: 'Successfully registered',
+          data: newUser,
+          error: '',
+        });
+      }
+    );
+  });
+};
+
+module.exports = AuthController;
---
+++ back/middlewares/auth/checkRegistration.js
@@ -0,0 +1,31 @@
+const { check, validationResult } = require('express-validator');
+
+const validateRegistrationDatas = [
+  check('email').isEmail().withMessage('Email not valid'),
+  check('firstName')
+    .isLength({ min: 2 })
+    .withMessage('must be at least 2 chars long'),
+  check('lastName')
+    .isLength({ min: 2 })
+    .withMessage('must be at least 2 chars long'),
+  check('password', 'The password must be 5+ chars long and contain a number')
+    .not()
+    .isIn(['123', 'password', 'god'])
+    .withMessage("N' utilisez pas un nom commun")
+    .isLength({ min: 5 })
+    .matches(/\d/),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(422).json({
+        success: false,
+        message: 'Something went wrong',
+        data: {},
+        error: `${errors.array()[0].param} ${errors.array()[0].msg}`,
+      });
+    }
+    return next();
+  },
+];
+
+module.exports = validateRegistrationDatas;
